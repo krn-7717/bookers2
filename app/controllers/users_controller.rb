0206d59c43c7604c1b5d42fd2c5dfbf36927e671
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-10-10 19:43:05 +0900

    そろそろ帰ろうかな

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
   def edit
